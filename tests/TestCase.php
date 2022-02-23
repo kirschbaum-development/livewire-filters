73f7388a94fbde8313cfc56ad4584e87c6869591
David VanScott <david@kirschbaumdevelopment.com>
2022-02-23 09:50:28 -0500

    Test case update

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -2,21 +2,11 @@
 
 namespace Kirschbaum\LivewireFilters\Tests;
 
-use Illuminate\Database\Eloquent\Factories\Factory;
 use Kirschbaum\LivewireFilters\LivewireFiltersServiceProvider;
 use Orchestra\Testbench\TestCase as Orchestra;
 
 class TestCase extends Orchestra
 {
-    protected function setUp(): void
-    {
-        parent::setUp();
-
-        Factory::guessFactoryNamesUsing(
-            fn (string $modelName) => 'Kirschbaum\\LivewireFilters\\Database\\Factories\\'.class_basename($modelName).'Factory'
-        );
-    }
-
     protected function getPackageProviders($app)
     {
         return [
@@ -27,10 +17,5 @@ class TestCase extends Orchestra
     public function getEnvironmentSetUp($app)
     {
         config()->set('database.default', 'testing');
-
-        /*
-        $migration = include __DIR__.'/../database/migrations/create_livewire-filters_table.php.stub';
-        $migration->up();
-        */
     }
 }
